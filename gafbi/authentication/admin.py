9fce3f50000949782cc61656f4b730d28c926797
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-02 10:30:12 +0600

    update admin for authentication

--- gafbi/authentication/admin.py
+++ gafbi/authentication/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User, OTP
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ["-created_at"]
+    list_display = ["email_address", "is_email_verified", "is_active", "is_staff", "created_at"]
+    search_fields = ["email_address"]
+
+    fieldsets = (
+        (None, {"fields": ("email_address", "password")}),
+        ("Status", {"fields": ("is_active", "is_email_verified", "is_staff", "is_superuser")}),
+        ("Permissions", {"fields": ("groups", "user_permissions")}),
+        ("Dates", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email_address", "password1", "password2", "is_staff", "is_superuser", "is_active", "is_email_verified"),
+        }),
+    )
+
+
+@admin.register(OTP)
+class OTPAdmin(admin.ModelAdmin):
+    list_display = ["email_address", "purpose", "otp_code", "is_verified", "attempt_count", "expires_at", "created_at"]
+    search_fields = ["email_address", "otp_code"]
+    list_filter = ["purpose", "is_verified"]
