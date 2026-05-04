bad9c24182964af45488af705698377c7305e4b8
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-04 12:53:28 +0600

    serializers create for faq

---
+++ gafbi/faqs/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import FAQ
+
+
+class FAQSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FAQ
+        fields = [
+            "id",
+            "question",
+            "answer",
+            "is_active",
+            "order",
+            "created_at",
+        ]
+        read_only_fields = ["id", "created_at"]
