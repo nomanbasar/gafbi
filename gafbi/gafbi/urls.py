5bb9e6befb2c2496c05f20c1e68c79a4537cfc9e
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-04 12:51:40 +0600

    root url include

--- gafbi/gafbi/urls.py
+++ gafbi/gafbi/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("api/auth/", include("authentication.urls")),
     path('api/products/', include('products.urls')),
     path("api/contacts/", include("contacts.urls")),
+    path("api/faqs/", include("faqs.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
