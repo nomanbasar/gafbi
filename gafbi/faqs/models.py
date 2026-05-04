a908fbb62502f8ec2956116429b6819c673befac
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-04 12:52:35 +0600

    faq model

--- gafbi/faqs/models.py
+++ gafbi/faqs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    is_active = models.BooleanField(default=True)
+    order = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["order", "-created_at"]
+
+    def __str__(self):
+        return self.question
