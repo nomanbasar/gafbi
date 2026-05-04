69ff9d45d6989c64387ffb4ba3de46f08a25e48f
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-04 10:35:51 +0600

    contact admin

--- gafbi/contacts/admin.py
+++ gafbi/contacts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "telephone_number",
+        "regarding",
+        "is_read",
+        "created_at",
+    )
+
+    list_filter = (
+        "is_read",
+        "created_at",
+    )
+
+    search_fields = (
+        "first_name",
+        "last_name",
+        "email",
+        "telephone_number",
+        "regarding",
+        "news",
+    )
+
+    readonly_fields = (
+        "created_at",
+    )
+
+    ordering = ("-created_at",)
