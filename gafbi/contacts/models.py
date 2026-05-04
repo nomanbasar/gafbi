2f09c853f6cfdf6c0d58ba63072fb5b3fe39a8e2
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-04 10:33:14 +0600

    contact models

--- gafbi/contacts/models.py
+++ gafbi/contacts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactMessage(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    telephone_number = models.CharField(max_length=30)
+    regarding = models.CharField(max_length=255)
+    news = models.TextField()
+
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.regarding}"
