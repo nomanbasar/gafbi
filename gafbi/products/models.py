551afe7bdcd60c9d7ff34547eae96ea4ef3a95e2
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-06 15:42:02 +0600

    review & rating for product

--- gafbi/products/models.py
+++ gafbi/products/models.py
@@ -32,3 +32,24 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+
+class ProductReview(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    rating = models.PositiveSmallIntegerField()
+    review = models.TextField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.product.name} - {self.rating}"
