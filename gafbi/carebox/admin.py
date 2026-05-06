e99a94dbde8a20daa4c6b99265dc03f6ed15e9d6
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-06 11:40:15 +0600

    admin for carebox

--- gafbi/carebox/admin.py
+++ gafbi/carebox/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import CareBoxApplication, CareBoxApplicationItem, CareBoxFeedback
 
-# Register your models here.
+
+class CareBoxApplicationItemInline(admin.TabularInline):
+    model = CareBoxApplicationItem
+    extra = 0
+    readonly_fields = ["product", "quantity", "unit_price", "subtotal"]
+
+
+@admin.register(CareBoxApplication)
+class CareBoxApplicationAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user",
+        "first_name",
+        "last_name",
+        "level_of_care",
+        "total_amount",
+        "application_month",
+        "status",
+        "created_at",
+    ]
+    list_filter = ["status", "application_month", "level_of_care", "created_at"]
+    search_fields = ["first_name", "last_name", "email", "phone_number"]
+    inlines = [CareBoxApplicationItemInline]
+
+
+@admin.register(CareBoxFeedback)
+class CareBoxFeedbackAdmin(admin.ModelAdmin):
+    list_display = ["id", "application", "user", "satisfaction", "information_helpful", "created_at"]
