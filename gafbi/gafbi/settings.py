3e06076a42f79b16506a0695862080ddbc7a1394
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-09 10:32:01 +0600

    update settings

--- gafbi/gafbi/settings.py
+++ gafbi/gafbi/settings.py
@@ -26,8 +26,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8l(q$508sg@eq+f0+wk%09fzmeg-p#j-eeh50q!e#8nyh__yr!'
-
+SECRET_KEY = os.getenv("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
